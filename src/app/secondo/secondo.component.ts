a9785dbee7a1f15d3ae7d8169f6c022b557f79a6
EmilioVerri <emilioverri83@gmail.com>
2020-03-18 11:15:38 +0100

    utilizzo event binding

--- src/app/secondo/secondo.component.ts
+++ src/app/secondo/secondo.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+/*import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-secondo',
@@ -10,6 +10,48 @@ export class SecondoComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  clickEvent(ev:MouseEvent,param: number){
+    if(param===2){
+
+    
+    this.testo='Default';
+  }
+    alert('ho cliccato il pulsante'+param);
+    ev.stopPropagation();
+  }
+
+  testo: string='Iniziare';
+  //inputEvent(ev){ lo sposto in s
+  //  this.testo=ev.target.value;
+  //}
+}
+*/
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-secondo',
+  templateUrl: './secondo.component.html',
+  styleUrls: ['./secondo.component.scss']
+})
+export class SecondoComponent implements OnInit {
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  clickEvent(ev: MouseEvent, param: number){
+    if(param === 2){
+      this.testo = 'Default';
+    }
+    console.log('ho cliccato il pulsante: '+param);
+    ev.stopPropagation();
+  }
+  testo: string = 'Iniziale';
+  inputEvent(ev){
+    this.testo = ev.target.value;
   }
 
 }
