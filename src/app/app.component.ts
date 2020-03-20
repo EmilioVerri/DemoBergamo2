e015dffa8136295413964a264aac852cdb79eb1b
EmilioVerri <emilioverri83@gmail.com>
2020-03-20 19:32:43 +0100

    utilizzo ngSwitch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  componentToShow: number=1
+
   title = 'Titolo';
 
   clickAppComponent(value:string){
     console.log('cliccato da secondo in app:'+value);
+  }
+
+  showComponent(){
+    if(this.componentToShow ===3){
+      this.componentToShow = 1;
+    }
+    else{
+      this.componentToShow++;
+    }
+    }
   }
   //title = 'DemoBergamo2';
 
@@ -20,4 +32,3 @@ export class AppComponent {
   htmlString='<div><p>Prova</div></p>';
   
   linkUrl='https://google.it';*/
-}
