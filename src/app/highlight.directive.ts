fae4ab335f6c145479845b35aa3ad4e27f8732ea
EmilioVerri <emilioverri83@gmail.com>
2020-04-01 23:51:57 +0200

    utilizzo proprietà di input

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,10 +1,10 @@
-import { Directive, ElementRef, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
-
+@Input() highlightColor: string;
   constructor(private el: ElementRef) { 
     //el.nativeElement.style.backgroundColor='yellow';
     //el.nativeElement.style.color='red';
