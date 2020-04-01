90a273ced271f325f4881d51099e7d93a72fdb3e
EmilioVerri <emilioverri83@gmail.com>
2020-04-01 23:43:39 +0200

    utilizzo ElementRef e HostListener

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,10 +1,22 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
-  constructor() { }
+  constructor(private el: ElementRef) { 
+    //el.nativeElement.style.backgroundColor='yellow';
+    //el.nativeElement.style.color='red';
+    //si possono tenere dentro ma non servono se c'è il mouse enter e leave
+  }
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.el.nativeElement.style.backgroundColor = 'yellow';
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.el.nativeElement.style.backgroundColor = null;
+  }
 
 }
