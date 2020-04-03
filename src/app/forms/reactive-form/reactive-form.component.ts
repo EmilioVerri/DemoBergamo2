0644d73e1d2d576db61265b459164dcc02d8ada4
EmilioVerri <emilioverri83@gmail.com>
2020-04-04 00:38:54 +0200

    creazione form innestato e accesso alle proprietà dei form control

--- src/app/forms/reactive-form/reactive-form.component.ts
+++ src/app/forms/reactive-form/reactive-form.component.ts
@@ -9,12 +9,43 @@ import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms'
 export class ReactiveFormComponent implements OnInit {
   powers: string[]=["Super velocità","Super forza","Indistruttibilità"];
 
+
+get nameControl():FormControl{
+  return this.heroForm.get('name') as FormControl;
+}
+
+get powerControl(): FormControl{
+  return this.heroForm.get('power') as FormControl;
+}
+
+setDefault() {
+  // in questo caso uso il patchValue, perché il setValue mi obbligherebbe ad inserire tutti i campi
+  // this.heroForm.setValue({
+  //   name: 'Goku'
+  // });
+  // this.heroForm.patchValue({
+  //   name: 'Goku'
+  // });
+  // in questo caso non c'è una reale differenza tra set e pathc Value
+  // this.nameControl.setValue('Goku');
+  this.nameControl.patchValue('Goku');
+}
+
+
+
   heroForm:FormGroup;
 
   constructor(private fb: FormBuilder) { 
     this.heroForm=this.fb.group({
       name:['',Validators.compose([Validators.required,Validators.minLength(2)])],
-      powe:['',Validators.required]
+      powe:['',Validators.required],
+      address:this.fb.group({
+        street:'',
+        city:'',
+        state:'',
+        zip: ['', Validators.compose([Validators.minLength(5),Validators.maxLength(5)])]
+      })
+     
     });
 
   }
